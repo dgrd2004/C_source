ea8c55008150886e4a6cb3257ec22e9beafb04fd
Edgardo Rodriguez <edgardo@debianLaptop.homenet.io>
2015-08-07 16:57:50 -0600

    improved summary of pointers and arrays in the pointer_summary.c file

--- pointer_summary.c
+++ pointer_summary.c
@@ -1,32 +1,22 @@
 #include <stdio.h>
 
 int main ( void ) {
-	const char constring[] = "This is a cosntant char array... 1 2 3...";
-	char string[] = "This is a char array...1 2 3...";
-	const int constint[] = {1, 10, 2, 3, 4, 5, 15, 6, 6, 6, 0 };
-	int integer[] = {11, 12, 13, 14, 14, 14, 14 };
-	char *pchar;
-	int *pint;
+//DECLARATION AND DEFINITION
+	char string[] = "This is a char array...1 2 3...";//ARRAY OF CHAR... ALSO KNOW AS STRING...
+	int integer[] = {1, 10, 2, 3, 4, 5, 6, 6, 6, 11, 12, 13, 14, 14, 14, 14, 15, 0 };//ARRAY OF INTS
+	char list[][64] = { "fist element", "second element", "third element", "...", "\0" };//ARRAY OF... ARRAYS OF A CONST CHAR... THIS CASE A 5X64 MATRIX OF CONST CHAR IS DECLARED... ALMOST EMPTY...
+	char *plist[][64] = { "fist element", "second element", "third element", "...", "\0" };//ARRAY OF... ARRAYS OF POINTER TO A CONST CHAR... THIS CASE ALSO A 5X64 MATRIX OF POINTERS TO A CHAR IS DECLARED AND DEFINED
+	char page[][1] = { 'c', 'h', 'a', 'r', 'a', 'c', 't', 'e', 'r', 's', ' ', 'o', 'r', 'd', 'e', 'n', 'e', 'd', '\n', '\0',
+			   't', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 'n', 'e', 'w', ' ', 'l', 'i', 'n', 'e', '\n', '\0' };//COLUMN 20X1 VECTOR OF CHARS
+	int matrix[][3] = { { 3, 2, 1 }, { 6, 5, 4 }, { 9, 8, 7 } };//MATRIX OF INTS
+//JUST DECLARATIONS
+	int *pmatrix[3][3];//3X3 MATRIX OF POINTERS TO INTS
+	char *pchar;//POINTER TO A CHAR
+	int *pint;//POINTER TO AN INT
 
 	int i = 0;
 	int j = 0;
-//	USING THE CONSTANT CHAR ARRAY 'constring'...
-	fprintf ( stdout, "const char address of 'constring' using it's own variable is: %p value is: %s\n\n", constring, constring );
-	while ( constring[i] != '\0' ) {
-		fprintf ( stdout, "const char address of 'constring[%i]' using it's own variable is: %p value is: %c\tcalculation #%i\n", i, &constring[i], constring[i], j );
-		i++;
-		j++;
-	}
-	i = 0;
-//	USING POINTED VALUES TO THE CONSTANT CHAR ARRAY WITH 'pchar' POINTER...
-	pchar = constring;
-	fprintf ( stdout, "const char address of 'constring' using pchar pointed to it, is: %p value is: %s\n\n", pchar, pchar );
-        while ( *(pchar + i) != '\0' ) {
-                fprintf ( stdout, "const char address of 'constring[%i]' using pchar pointed to 'pchar + %i' is: %p value is: %c\tcalculation #%i\n", i, i, pchar + i, *(pchar + i), j );
-		i++;
-		j++;
-        }
-        i = 0;
+
 //	USING THE CHAR ARRAY 'string'...
 	fprintf ( stdout, "the address of 'constring' using it's own variable is: %p value is: %s\n\n", string, string );
         while ( string[i] != '\0' ) {
@@ -44,22 +34,24 @@ int main ( void ) {
                 j++;
         }
         i = 0;
-//	USING THE CONSTANT INT ARRAY 'constint'...
-	fprintf ( stdout, "the addres of 'constint' using it's own variable is: %p value is: %i\n\n", constint, constint[i] );
-	while ( constint[i] != 0 ) {
-		fprintf ( stdout, "the address of 'constint[%i]' using it's own variable is: %p value is: %i\tcalculation #%i\n", i, &constint[i], constint[i], j );
+//	USING THE INT ARRAY 'integer'...
+	fprintf ( stdout, "the addres of 'integer' using it's own variable is: %p value is: %i\n\n", integer, integer[i] );
+	while ( integer[i] != 0 ) {
+		fprintf ( stdout, "the address of 'integer[%i]' using it's own variable is: %p value is: %i\tcalculation #%i\n", i, &integer[i], integer[i], j );
 		i++;
 		j++;
 	}
 	i = 0;
-//      USING POINTED VALUES TO THE CONSTANT INT ARRAY 'constint' WITH THE 'pint' POINTER...
-	pint = constint;
-        fprintf ( stdout, "the addres of 'constint' using pint pointed to it, is: %p value is: %i\n\n", pint, *(pint + i) );
+//      USING POINTED VALUES TO THE INT ARRAY 'integer' WITH THE 'pint' POINTER...
+	pint = integer;
+        fprintf ( stdout, "the addres of 'integer' using pint pointed to it, is: %p value is: %i\n\n", pint, *(pint + i) );
         while ( *(pint + i) != 0 ) {
-                fprintf ( stdout, "the address of 'constint[%i]' using pint pinted to 'pint + %i' is: %p value is: %i\tcalculation #%i\n", i, i, pint + 1, *(pint + i), j );
+                fprintf ( stdout, "the address of 'integer[%i]' using pint pinted to 'pint + %i' is: %p value is: %i\tcalculation #%i\n", i, i, pint + 1, *(pint + i), j );
                 i++;
                 j++;
         }
         i = 0;
+//	USING POINTER TO A CHAR 'list'  ARRAY OF ARRAYS...
+	fprintf ( stdout, "the address of 'list' using it's own variable is: %p value is: %s\n", list, list );
 	return 0;
 }
